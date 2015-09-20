d35b88185104ff2be976bb5ff0bb2474cbf49da4
gauthier <gauthier@objective-php.org>
2015-09-20 17:08:27 +0200

    Add methods to REsponse interface

--- src/Response/HttpResponse.php
+++ src/Response/HttpResponse.php
@@ -1,17 +1,17 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: gauthier
-     * Date: 10/08/15
-     * Time: 18:16
-     */
-    
+
     namespace ObjectivePHP\Message\Response;
     
     
     use Zend\Diactoros\Response;
+    use Zend\Diactoros\Stream;
 
+    /**
+     * Class HttpResponse
+     *
+     * @package ObjectivePHP\Message\Response
+     */
     class HttpResponse extends Response implements ResponseInterface
     {
-        
+
     }
--- src/Response/ResponseInterface.php
+++ src/Response/ResponseInterface.php
@@ -2,7 +2,18 @@
     
     namespace ObjectivePHP\Message\Response;
 
+    use Psr\Http\Message\StreamInterface;
+
+    /**
+     * Interface ResponseInterface
+     *
+     * @package ObjectivePHP\Message\Response
+     */
     interface ResponseInterface
     {
-        
+        /**
+         * @return StreamInterface
+         */
+        public function getBody();
+
     }
